9d4633b7cf9e975b1518930ab4dfb2fa712bf16c
Faqihza <faqihza.m@gmail.com>
2019-08-19 08:25:41 +1000

    update episode 59

---
+++ 59-Scanner/src/com/tutorial/Main.java
@@ -0,0 +1,87 @@
+package com.tutorial;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        FileReader fileInput = new FileReader("input.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileInput);
+
+        Scanner scanner = new Scanner(bufferedReader);
+
+        // ini untuk cek ada kata selanjutnya atau tidak
+        System.out.println(scanner.hasNext()); // true
+
+        // untuk membaca kata, dengan pemisah/delimeter spasi
+        System.out.println(scanner.next());
+        System.out.println(scanner.next());
+        System.out.println(scanner.next());
+        System.out.println(scanner.next());
+        System.out.println(scanner.next());
+
+        System.out.println(scanner.hasNext()); // false
+
+        // Menggunakan delimiter tertentu
+        FileReader fileInput2 = new FileReader("input2.txt");
+        bufferedReader = new BufferedReader(fileInput2);
+        bufferedReader.mark(200);
+
+        scanner = new Scanner(bufferedReader);
+        scanner.useDelimiter(",");
+
+        while(scanner.hasNext()){
+            System.out.println(scanner.next());
+        }
+
+        // menggunakan String tokenizer
+
+        bufferedReader.reset();
+        String data = bufferedReader.readLine();
+        System.out.println(data);
+
+        StringTokenizer stringToken = new StringTokenizer(data,",");
+
+        while(stringToken.hasMoreTokens()){
+            System.out.println(stringToken.nextToken());
+        }
+
+        // baris kedua
+        data = bufferedReader.readLine();
+        System.out.println(data);
+
+        stringToken = new StringTokenizer(data,",");
+
+        while(stringToken.hasMoreTokens()){
+            System.out.println(stringToken.nextToken());
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
